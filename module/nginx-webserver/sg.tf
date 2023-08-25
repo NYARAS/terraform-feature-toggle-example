d4660e94771b935c619168d39159d6c6e52d9bbe
Calvine Otieno <nyarangaotieno@gmail.com>
2023-08-25 19:49:54 +0300

    feat: 🌱 added nginx webserver infra setup as a module
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ module/nginx-webserver/sg.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "sg-nginx-webserver" {
+  for_each        = var.security_groups_map
+  name            = each.value
+  description = "Web server security group."
+  ingress {
+    description = "Allow port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all IP and Ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/1"]
+  }
+}
---
+++ module/nginx-webserver/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID"
+  type        = string
+  default     = "ami-04dd4500af104442f" # Amazon Linus 2 AMI ID
+}
+
+variable "ec2_instance_count" {
+  description = "EC2 Instance Count"
+  type        = number
+  default     = 1
+}
+
+variable "security_groups_map" {
+  description = "A map of security groups to be created"
+  type = map
+  default = {
+      test = "test"
+  }
+}
