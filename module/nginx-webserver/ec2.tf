25c13114add9ca9f70994c9b68c332ba8680ab70
Calvine Otieno <nyarangaotieno@gmail.com>
2023-08-28 22:17:56 +0300

    fix: add subnet count
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- module/nginx-webserver/ec2.tf
+++ module/nginx-webserver/ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "nginx-webserver" {
+    count                  = var.ec2_instance_count
   ami                    = data.aws_ami.amzlinux.id
   instance_type          = var.instance_type
-  subnet_id              = local.subnets
+  subnet_id              = element(local.subnets, count.index)
   vpc_security_group_ids = [aws_security_group.nginx-webserver.id]
 
-   user_data = templatefile("./nginx-script.sh", {
+   user_data = templatefile("./module/nginx-webserver/nginx-script.sh", {
     file_content = "Nginx Webserver"
   })
 
